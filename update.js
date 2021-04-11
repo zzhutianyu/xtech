2425f77458933cbee43053393604f0c397c2b0ab
zhutianyu <704735206@qq.com>
2021-04-11 21:07:21 +0800

    feature: 添加脚步，以及更新内容

---
+++ update.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
+function walk (dir, record) {
+    fs.readdirSync(dir).forEach(fileOrDir => {
+        if (fs.statSync(path.join(dir, fileOrDir)).isDirectory()) walk(path.join(dir, fileOrDir), record)
+
+        if (fileOrDir.endsWith(".json")) {
+            if (['database.json', 'database.schema.json', 'graph.schema.json', '.eslintrc.json', 'package-lock.json', 'package.json'].includes(fileOrDir)) {
+                return
+            }
+            record.push(path.join(dir, fileOrDir))
+        }
+    });
+    return record
+}
+
+let result = []
+walk('./', result).forEach(item => console.log(item))
+
+const dst = JSON.stringify(result, null, 2)
+fs.writeFileSync('database.json', dst)
